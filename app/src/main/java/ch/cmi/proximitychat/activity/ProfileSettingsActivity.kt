1eedae71b6128b5e6d4ee17f5e7ddcdfe38b7e39
rstadler6 <54070175+rstadler6@users.noreply.github.com>
2021-11-18 23:30:20 +0100

    Profile Settings layout

--- app/src/main/java/ch/cmi/proximitychat/activity/ProfileSettingsActivity.kt
+++ app/src/main/java/ch/cmi/proximitychat/activity/ProfileSettingsActivity.kt
@@ -1,21 +1,31 @@
 package ch.cmi.proximitychat.activity
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import ch.cmi.proximitychat.R
 
 class ProfileSettingsActivity : AppCompatActivity() {
+    private lateinit var icon: ImageView
+    private lateinit var username: TextView
+    private val prefs = getPreferences(Context.MODE_PRIVATE)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_settings)
+
+        icon = findViewById(R.id.icon)
+        username = findViewById(R.id.username)
     }
 
     fun back (view: View?) {
         finish()
     }
 
-    fun changePicture(view: View?) {
+    fun changeIcon(view: View?) {
 
     }
 
