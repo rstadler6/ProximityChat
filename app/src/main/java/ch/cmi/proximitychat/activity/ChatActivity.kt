df5cacf6ae3ad97c1c9231a538b9160de377bfb0
Raphael Stadler <54070175+rstadler6@users.noreply.github.com>
2021-11-18 14:48:40 +0100

    Chat Layout

--- app/src/main/java/ch/cmi/proximitychat/activity/ChatActivity.kt
+++ app/src/main/java/ch/cmi/proximitychat/activity/ChatActivity.kt
@@ -2,11 +2,16 @@ package ch.cmi.proximitychat.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import ch.cmi.proximitychat.R
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+    }
+
+    fun back (view: View?) {
+        finish()
     }
 }
